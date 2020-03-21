cedafb050efb121baa6c213f40c26f86fe43c939
nojaf <florian.verdonck@outlook.com>
2020-03-21 18:41:41 +0100

    Use the name from the script in the library.

--- test/Library.fs
+++ test/Library.fs
@@ -5,5 +5,5 @@ open Newtonsoft.Json
 
 module Say =
     let hello name =
-        let o = {| X = 2; Y = "Hello" |}
+        let o = {| X = 2; Y = sprintf "Hello %s" name |}
         sprintf "%s" (JsonConvert.SerializeObject o)
