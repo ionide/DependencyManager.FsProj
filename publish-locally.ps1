b4f446a780593856bed04cf515086651ac5084be
Tomas Leko <tomas.leko@gmail.com>
2022-04-03 07:30:07 +0200

    delete output dir before deploying locally

--- publish-locally.ps1
+++ publish-locally.ps1
@@ -1,6 +1,7 @@
 $workdir=".workdir"
 $outdir=".depman"
 Remove-Item -Recurse -ErrorAction SilentlyContinue $workdir
+Remove-Item -Recurse -ErrorAction SilentlyContinue $outdir
 dotnet new classlib -o $workdir -n depman
 dotnet add $workdir reference ./src/DependencyManager.FsProj.fsproj
 dotnet publish $workdir -o $outdir
